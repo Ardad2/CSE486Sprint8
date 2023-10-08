78fb1de7e05a41108453bbd127b84c58acd381b2
maxquadracci <maxquad123@gmail.com>
2023-10-07 17:21:37 -0700

    getting ready to add profile screen
    making sure it is consistent throughout the app

--- screens/ProfileScreen/ProfileScreen.js
+++ screens/ProfileScreen/ProfileScreen.js
@@ -1,1 +1,15 @@
 //getting ready to add profile
+import {useState, useEffect} from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { useSelector, dispatch, useDispatch } from 'react-redux';
+
+import { 
+  StyleSheet,  
+  View, 
+  FlatList, Button, Text
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+//this will be the screen for the profile
+//importing the same status bar so it is consistent throughout the app.
