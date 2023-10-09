95d38b15c8ce1c2a32ea1e4ef1292d9105fc58c8
Mazhar Bilgrami <70132390+Maz-Bil@users.noreply.github.com>
2023-10-08 17:16:01 -0700

    Added comments to behavior functions

--- components/BehaviorInput.js
+++ components/BehaviorInput.js
@@ -1,43 +1,43 @@
-import { useState } from 'react';
-import { StyleSheet, View, TextInput, Button, Modal, Image } from 'react-native';
+import { useState } from 'react'; //Note: useState is a React Hook
+import { StyleSheet, View, TextInput, Button, Modal, Image } from 'react-native'; //Note: Modal is a component that is built into React Native
 
-function BehaviorInput(props) {
+function BehaviorInput(props) { //Note: this is a function that is passed in as a prop from App.js
 
     const [enteredBehaviorText, setEnteredBehaviorText] = useState('');
 
-    function behaviorInputHandler(enteredText) {
+    function behaviorInputHandler(enteredText) { //Note: this is a function that is passed in as a prop from App.js
         setEnteredBehaviorText(enteredText);
-      };
+    };
 
-      function addBehaviorHandler() {
-          props.onAddBehavior(enteredBehaviorText);
-          setEnteredBehaviorText('');
-      }
+    function addBehaviorHandler() { //Note: this is a function that is passed in as a prop from App.js
+        props.onAddBehavior(enteredBehaviorText);
+        setEnteredBehaviorText('');
+    }
 
-    return (
+    return ( //Note: this is a function that is passed in as a prop from App.js
         <Modal visible={props.visible} animationType="slide">
-        <View style ={styles.inputContainer}>
-            <Image style={styles.image} source={require('../assets/goal.png')}/>
-        <TextInput 
-        style={styles.textInput} 
-        placeholder="What did you do today?"
-        
-        //Note if it was goalInputHandler() it would get executed as soon as React starts.
+            <View style ={styles.inputContainer}>
+                <Image style={styles.image} source={require('../assets/goal.png')}/>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="What did you do today?" 
 
-         onChangeText={behaviorInputHandler}
-         value={enteredBehaviorText}
-         />
-         <View style={styles.buttonContainer}>
-             <View style={styles.button} >
-                <Button title="Add Behavior" onPress={addBehaviorHandler} color="black"/>
-            </View>
-            <View style={styles.button}>
-                <Button title="Cancel" onPress={props.onCancel} color="black"/>
-            </View>
-        </View>
-      </View>
+                    //Note if it was goalInputHandler() it would get executed as soon as React starts.
 
-      </Modal>
+                    onChangeText={behaviorInputHandler}
+                    value={enteredBehaviorText}
+                />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button} >
+                        <Button title="Add Behavior" onPress={addBehaviorHandler} color="black"/>
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Cancel" onPress={props.onCancel} color="black"/>
+                    </View>
+                </View>
+            </View>
+
+        </Modal>
     )
 };
 
@@ -45,11 +45,11 @@ export default BehaviorInput;
 
 const styles = StyleSheet.create({
     inputContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 16,
-      backgroundColor: 'white'
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: 'white'
     },
     image: {
         width: 100,
@@ -57,13 +57,13 @@ const styles = StyleSheet.create({
         margin: 20
     },
     textInput: {
-      borderWidth: 1,
-      borderColor: '#e4d0ff',
-      backgroundColor: '#e4d0ff',
-      color: '#120438',
-      borderRadius: 6,
-      width: '100%',
-      padding: 16
+        borderWidth: 1,
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        color: '#120438',
+        borderRadius: 6,
+        width: '100%',
+        padding: 16
     },
     buttonContainer: {
         marginTop: 16,
@@ -73,5 +73,4 @@ const styles = StyleSheet.create({
         width: 100,
         marginHorizontal: 8
     }
-  });
-  
+});
